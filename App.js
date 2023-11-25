5e2b9ec98e51fe2326cfa8674355c41570232220
minhhoanglazy <minhhoanglazy@gmail.com>
2023-11-25 11:23:34 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import HomeScreen from './Home';
+import ScanScreen from './Scan';
+import CartScreen from './Cart';
+
+// Placeholder screen component
+const PlaceholderScreen = ({ route }) => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>{route.name} Screen</Text>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={{
+          headerShown: false,
+        }}
+      tabBarOptions={{
+          showLabel: false,}}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Image source={focused ? require('./assets/HomeActive.png') : require('./assets/Home.png')} style={styles.logo} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Notification" 
+          component={PlaceholderScreen} 
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/Bell.png')} style={styles.logo} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Scan New" 
+          component={ScanScreen} 
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/Vector.png')} style={styles.logo} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Recent" 
+          component={PlaceholderScreen} 
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/Recent.png')} style={styles.logo} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Cart" 
+          component={CartScreen} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Image source={focused ? require('./assets/CartActive.png') : require('./assets/Cart.png')} style={styles.logo} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  logo:{
+    width: 24, 
+    height: 24,
+    resizeMode: 'contain',
   },
-});
+}) 
+
+export default App;
